7911fedda1d3f20db07ad181a67988acc73a9141
Alexander4881 <alihnmikkelsen@gmail.com>
2020-04-24 11:55:07 +0200

    Fifth Driver Alexander

--- app/src/main/java/com/example/rgb_converter/MainActivity.java
+++ app/src/main/java/com/example/rgb_converter/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.rgb_converter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.rgb_converter.Interfaces.Presenter;
 import com.example.rgb_converter.Interfaces.View;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements View<RGBData> {
     private Presenter _presenter;
     private SeekBar _seekBarRed, _seekBarBlue, _seekBarGreen;
     private ImageView _imageView;
-    private VectorDrawable _circleBlend;
+    private TextView _textViewHex;
 
 
     @Override
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View<RGBData> {
         _seekBarBlue = findViewById(R.id.seekBarBlue);
         _seekBarGreen = findViewById(R.id.seekBarGreen);
         _imageView = findViewById(R.id.imageView);
+        _textViewHex = findViewById(R.id.textViewHex);
 
         _seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -66,6 +69,11 @@ public class MainActivity extends AppCompatActivity implements View<RGBData> {
     public void update(RGBData rgbData) {
 
         //(VectorDrawable)_imageView.getDrawable().setTint();
+        _imageView.setColorFilter(Color.argb(255, rgbData.getRed(), rgbData.getGreen(), rgbData.getGreen()));
+
+        String hex = String.format("#%02x%02x%02x", rgbData.getRed(), rgbData.getGreen(), rgbData.getGreen());
+
+        _textViewHex.setText(hex);
 
         Log.e("", "update called");
         // TODO update view thing rgb box
