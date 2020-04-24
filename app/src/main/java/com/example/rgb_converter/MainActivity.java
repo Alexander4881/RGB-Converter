404241e370d25b774f04055a8b6e1123f93508dd
MDW91 <42375451+MDW91@users.noreply.github.com>
2020-04-24 11:06:44 +0200

    thirddriver

--- app/src/main/java/com/example/rgb_converter/MainActivity.java
+++ app/src/main/java/com/example/rgb_converter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.rgb_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 import com.example.rgb_converter.Interfaces.Presenter;
 import com.example.rgb_converter.Interfaces.View;
@@ -10,17 +12,41 @@ import com.example.rgb_converter.Interfaces.View;
 public class MainActivity extends AppCompatActivity implements View<RGBData> {
 
     private Presenter _presenter;
+    private SeekBar seekBarRed, seekBarBlue, seekBarGreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         _presenter = new MainActivityPresenter(this);
+
+        // view injection
+        seekBarRed = findViewById(R.id.seekBarRed);
+        seekBarBlue = findViewById(R.id.seekBarBlue);
+        seekBarGreen = findViewById(R.id.seekBarGreen);
+
+        seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.e("", "onProgressChanged: " +progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+
 
     @Override
     public void update(RGBData rgbData) {
         // TODO update view thing rgb box
-
     }
 }
