cf1aa98a5f01d99fb6465042fc694418aadb5128
KMRH47 <kramze47@gmailcom>
2020-04-24 11:37:18 +0200

    fourth driver

--- app/src/main/java/com/example/rgb_converter/MainActivity.java
+++ app/src/main/java/com/example/rgb_converter/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.rgb_converter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import com.example.rgb_converter.Interfaces.Presenter;
@@ -12,7 +14,10 @@ import com.example.rgb_converter.Interfaces.View;
 public class MainActivity extends AppCompatActivity implements View<RGBData> {
 
     private Presenter _presenter;
-    private SeekBar seekBarRed, seekBarBlue, seekBarGreen;
+    private SeekBar _seekBarRed, _seekBarBlue, _seekBarGreen;
+    private ImageView _imageView;
+    private VectorDrawable _circleBlend;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +26,48 @@ public class MainActivity extends AppCompatActivity implements View<RGBData> {
         _presenter = new MainActivityPresenter(this);
 
         // view injection
-        seekBarRed = findViewById(R.id.seekBarRed);
-        seekBarBlue = findViewById(R.id.seekBarBlue);
-        seekBarGreen = findViewById(R.id.seekBarGreen);
+        _seekBarRed = findViewById(R.id.seekBarRed);
+        _seekBarBlue = findViewById(R.id.seekBarBlue);
+        _seekBarGreen = findViewById(R.id.seekBarGreen);
+        _imageView = findViewById(R.id.imageView);
 
-        seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        _seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                _presenter.UpdateRed((byte)progress);
                 Log.e("", "onProgressChanged: " +progress);
             }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+        _seekBarBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                _presenter.UpdateBlue((byte)progress);
+
+                Log.e("", "onProgressChanged: " +progress);
+            }
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+        _seekBarGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                _presenter.UpdateGreen((byte)progress);
+                Log.e("", "onProgressChanged: " +progress);
+            }
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
     }
-
-
 
     @Override
     public void update(RGBData rgbData) {
+
+        //(VectorDrawable)_imageView.getDrawable().setTint();
+
+        Log.e("", "update called");
         // TODO update view thing rgb box
     }
 }
